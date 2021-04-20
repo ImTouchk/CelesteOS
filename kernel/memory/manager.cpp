3671ca6a2ff7793cbafb9974b2cafd0ab5d17a68
imtouchk <imtouchk@gmail.com>
2021-04-20 20:04:00 +0300

    Memory: Bundle everything related into Memory::runtime struct.

--- kernel/memory/manager.cpp
+++ kernel/memory/manager.cpp
@@ -2,6 +2,44 @@
 #include "../boot.hpp"
 #include "memory.hpp"
 
-namespace Memory {
+using size_t = unsigned long;
 
+extern usize __kernelEnd;
+extern usize __kernelStart;
+
+namespace Memory {
+    runtime::runtime(Boot::data& bootData, BasicTerminal& terminal)
+    {
+        /* Load global descriptor table */
+        Memory::gdtDescriptor descriptor = {
+                .size   = sizeof(Memory::gdt) - 1,
+                .offset = (usize)&Memory::descriptorTable
+        };
+        load_gdt(&descriptor);
+
+        /* Set-up paging */
+        const usize kernelSize  = (usize)&__kernelEnd - (usize)&__kernelStart;
+        const usize kernelPages = kernelSize / 4096 + 1;
+
+        frameAllocator = pageFrameAllocator(bootData.memoryData);
+        frameAllocator.lock(&__kernelStart, kernelPages);
+
+        pPML4 = (pageTable*)frameAllocator.request();
+        Memory::set(pPML4, 0x1000, 0x00);
+
+        tableManager = pageTableManager(pPML4, &frameAllocator);
+        for(usize i = 0; i < frameAllocator.totalMemory(); i += 0x1000) {
+            tableManager.map((void*)i, (void*)i);
+        }
+
+        const usize bufferBase = (usize)bootData.pScreenData->pFrontBuffer;
+        const usize bufferSize = (usize)bootData.pScreenData->bufferSize + 0x1000;
+        frameAllocator.lock((void*)bufferBase, bufferSize / 0x10000 + 1);
+        for(usize i = bufferBase; i < (bufferBase + bufferSize); i += 4096) {
+            tableManager.map((void*)i, (void*)i);
+        }
+
+        __asm("mov %0, %%cr3" : : "r" (pPML4));
+        /* ^--- switch to the new table manager */
+    }
 }
--- kernel/memory/memory.hpp
+++ kernel/memory/memory.hpp
@@ -3,6 +3,7 @@
 
 #define x86_64 1
 
+#include "../boot.hpp"
 #include "paging/map_indexer.hpp"
 #include "paging/frame_allocator.hpp"
 #include "paging/table_manager.hpp"
@@ -11,5 +12,15 @@
 #ifdef x86_64
 #   include "x86_64/gdt.hpp"
 #endif
+
+namespace Memory {
+    struct runtime {
+        runtime(Boot::data& bootData, BasicTerminal& terminal);
+
+        pageFrameAllocator frameAllocator;
+        pageTableManager tableManager;
+        pageTable* pPML4;
+    };
+}
 
 #endif // MEMORY_H
