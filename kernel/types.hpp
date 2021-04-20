592a223a08726a143c4c31ca9fbf193ee42c4113
imtouchk <imtouchk@gmail.com>
2021-04-20 15:09:43 +0300

    Kernel: Added 16-bit data typedefs.

--- kernel/types.hpp
+++ kernel/types.hpp
@@ -2,8 +2,11 @@
 #define TYPES_HPP
 
 using byte = unsigned char;
+using u16  = unsigned short;
 using u32  = unsigned int;
 using u64  = unsigned long long;
+
+using i16  = short;
 using i32  = int;
 using i64  = long long;
 using f32  = float;
