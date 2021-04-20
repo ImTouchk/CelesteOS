bdd7c7df2c95fdf05efed3cfa66f0e5f6e604995
imtouchk <imtouchk@gmail.com>
2021-04-20 17:53:58 +0300

    Build: Assembly file support.

---
+++ kernel/memory/x86_64/gdt.hpp
@@ -0,0 +1,34 @@
+#ifndef MEMORY_GDT_H
+#define MEMORY_GDT_H
+
+#include "../../types.hpp"
+
+namespace Memory {
+    struct gdtDescriptor {
+        u16 size;
+        usize offset;
+    } __attribute__((packed));
+
+    struct gdtEntry {
+        u16 limit0;
+        u16 base0;
+        byte base1;
+        byte accessByte;
+        byte limit1Flags;
+        byte base2;
+    } __attribute__((packed));
+
+    struct gdt {
+        gdtEntry null;
+        gdtEntry kernelCode;
+        gdtEntry kernelData;
+        gdtEntry userNull;
+        gdtEntry userCode;
+        gdtEntry userData;
+    } __attribute__((packed))
+    __attribute__((aligned(0x1000)));
+
+    extern gdt descriptorTable;
+}
+
+#endif // MEMORY_GDT_H
